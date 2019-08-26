863b0ad5ed0761a5ffc899424bcef5a01079cb35
taisandog <taisandog@gmail.com>
2019-08-26 17:31:17 +0800

    添加数据库支持

---
+++ SourceCodeCore/BuffaloCore/Buffalo.Data.Oracle/AdapterLoader.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Buffalo.DB.DataBaseAdapter.IDbAdapters;
+
+namespace Buffalo.Data.Oracle
+{
+    /// <summary>
+    ///   ≈‰∆˜º”‘ÿ∆˜
+    /// </summary>
+    public class AdapterLoader : IAdapterLoader
+    {
+        /// <summary>
+        /// µ±«∞ ˝æ›ø‚  ≈‰∆˜
+        /// </summary>
+        public IDBAdapter DbAdapter 
+        {
+            get 
+            {
+                if(_version=="11" || _version == "12")
+                {
+                    return new DBAdapter11();
+                }
+                return new DBAdapter();
+            }
+        }
+
+        public IAggregateFunctions AggregateFunctions 
+        {
+            get
+            {
+                return new AggregateFunctions();
+            }
+        }
+
+        public IMathFunctions MathFunctions 
+        {
+            get 
+            {
+                return new MathFunctions();
+            }
+        }
+        public IConvertFunction ConvertFunctions 
+        {
+            get 
+            {
+                return new ConvertFunction();
+            }
+        }
+        public ICommonFunction CommonFunctions 
+        {
+            get 
+            {
+                return new CommonFunction();
+            }
+        }
+
+        public IDBStructure DBStructure
+        {
+            get
+            {
+                return new DBStructure();
+            }
+        }
+        private string _version;
+        public void SetDBVersion(string version)
+        {
+            _version = version;
+        }
+    }
+}
---
+++ SourceCodeCore/BuffaloCore/Buffalo.Data.Oracle/ConvertFunction.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Buffalo.Data.Oracle
+{
+    /// <summary>
+    /// 数值转换函数
+    /// </summary>
+    public class ConvertFunction :Buffalo.DB.DataBaseAdapter.IDbAdapters.IConvertFunction
+    {
+
+        /// <summary>
+        /// 格式化字符串
+        /// </summary>
+        /// <returns></returns>
+        private static string GetFormat(string format)
+        {
+            StringBuilder ret = new StringBuilder(format);
+            ret.Replace("hh", "hh24");
+            ret.Replace("ms", "ff3");
+            ret.Replace("mm", "mi");
+            return ret.ToString();
+        }
+        /// <summary>
+        /// 日期转字符串
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public string DateTimeToString(string dateTime, string format) 
+        {
+            if (format != null) 
+            {
+                return "to_char(" + dateTime + ",'" + GetFormat(format) + "')";
+            }
+            return "to_char(" + dateTime + ",'yyyy-mm-dd hh24-mi-ss.ff3')";
+        }
+
+        /// <summary>
+        /// 字符串转成日期
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public string StringToDateTime(string value,string format)
+        {
+            if (format != null)
+            {
+                return "to_timestamp(" + value + ",'" + GetFormat(format) + "')";
+            }
+            return "to_timestamp(" + value + ",'yyyy-mm-dd hh24-mi-ss.ff3')";
+        }
+        /// <summary>
+        /// 把数据转成指定类型
+        /// </summary>
+        /// <param name="value">数据</param>
+        /// <param name="dbType">指定类型</param>
+        /// <returns></returns>
+        public string ConvetTo(string value, DbType type)
+        {
+            if (type == DbType.AnsiString || type == DbType.AnsiStringFixedLength || type == DbType.String ||
+                type == DbType.StringFixedLength) 
+            {
+                return "TO_CHAR(" + value + ")";
+            }else if (type == DbType.Int64 ||
+                type == DbType.Decimal || type == DbType.Double || type == DbType.Int32 ||
+            type == DbType.Int16 || type == DbType.Double ||
+            type == DbType.SByte || type == DbType.Byte || type == DbType.Currency || type == DbType.UInt16
+            || type == DbType.UInt32 || type == DbType.UInt64 || type == DbType.VarNumeric || type == DbType.Boolean
+                )
+            {
+                return "TO_NUMBER(" + value + ")";
+            }
+            else if (type == DbType.DateTime || type == DbType.Time || type == DbType.Date)
+            {
+                return "TO_NUMBER(" + value + ")";
+            }
+            return value;
+        }
+    }
+}
