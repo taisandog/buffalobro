c1f57001e2f49578c648649ced81a1b56667d77c
taisandog@gmail.com <taisandog@gmail.com>
2021-09-01 18:54:59 +0800

    升级安装器，以支持新的RedisMQ特性

--- OtherPorject/AddInSetup/AddInSetup/ConnStringUI/UIRedisMQ.Designer.cs
+++ OtherPorject/AddInSetup/AddInSetup/ConnStringUI/UIRedisMQ.Designer.cs
@@ -39,16 +39,25 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.chkUseQueue = new System.Windows.Forms.CheckBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.cmbCommandFlags = new System.Windows.Forms.ComboBox();
             this.label10 = new System.Windows.Forms.Label();
             this.txtDatabase = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cmbMessageMode = new System.Windows.Forms.ComboBox();
+            this.cmbCommandFlags = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.nupMessageMode = new System.Windows.Forms.NumericUpDown();
             this.gpSetting.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.txtExpir)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtDatabase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupMessageMode)).BeginInit();
             this.SuspendLayout();
             // 
             // gpSetting
             // 
+            this.gpSetting.Controls.Add(this.label12);
+            this.gpSetting.Controls.Add(this.nupMessageMode);
+            this.gpSetting.Controls.Add(this.label11);
+            this.gpSetting.Controls.Add(this.cmbMessageMode);
             this.gpSetting.Controls.Add(this.label10);
             this.gpSetting.Controls.Add(this.txtDatabase);
             this.gpSetting.Controls.Add(this.label9);
@@ -68,7 +77,7 @@
             // chkSSL
             // 
             this.chkSSL.AutoSize = true;
-            this.chkSSL.Location = new System.Drawing.Point(193, 243);
+            this.chkSSL.Location = new System.Drawing.Point(194, 324);
             this.chkSSL.Name = "chkSSL";
             this.chkSSL.Size = new System.Drawing.Size(55, 25);
             this.chkSSL.TabIndex = 64;
@@ -79,7 +88,7 @@
             // 
             this.txtPwd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPwd.Location = new System.Drawing.Point(194, 101);
+            this.txtPwd.Location = new System.Drawing.Point(194, 104);
             this.txtPwd.Name = "txtPwd";
             this.txtPwd.Size = new System.Drawing.Size(457, 29);
             this.txtPwd.TabIndex = 63;
@@ -88,7 +97,7 @@
             // 
             this.txtExpir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtExpir.Location = new System.Drawing.Point(194, 138);
+            this.txtExpir.Location = new System.Drawing.Point(194, 139);
             this.txtExpir.Maximum = new decimal(new int[] {
             999999999,
             0,
@@ -102,7 +111,7 @@
             // 
             this.txtServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtServer.Location = new System.Drawing.Point(194, 64);
+            this.txtServer.Location = new System.Drawing.Point(194, 69);
             this.txtServer.Name = "txtServer";
             this.txtServer.Size = new System.Drawing.Size(457, 29);
             this.txtServer.TabIndex = 60;
@@ -111,7 +120,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(2, 63);
+            this.label5.Location = new System.Drawing.Point(2, 69);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(184, 21);
             this.label5.TabIndex = 65;
@@ -120,7 +129,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(140, 104);
+            this.label6.Location = new System.Drawing.Point(140, 105);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(46, 21);
             this.label6.TabIndex = 66;
@@ -138,7 +147,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(124, 29);
+            this.label8.Location = new System.Drawing.Point(124, 33);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(62, 21);
             this.label8.TabIndex = 69;
@@ -148,7 +157,7 @@
             // 
             this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtName.Location = new System.Drawing.Point(193, 29);
+            this.txtName.Location = new System.Drawing.Point(193, 34);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(457, 29);
             this.txtName.TabIndex = 68;
@@ -159,7 +168,7 @@
             this.chkUseQueue.AutoSize = true;
             this.chkUseQueue.Checked = true;
             this.chkUseQueue.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkUseQueue.Location = new System.Drawing.Point(193, 274);
+            this.chkUseQueue.Location = new System.Drawing.Point(194, 355);
             this.chkUseQueue.Name = "chkUseQueue";
             this.chkUseQueue.Size = new System.Drawing.Size(157, 25);
             this.chkUseQueue.TabIndex = 70;
@@ -170,11 +179,55 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.label9.Location = new System.Drawing.Point(53, 212);
+            this.label9.Location = new System.Drawing.Point(54, 284);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(133, 21);
             this.label9.TabIndex = 72;
             this.label9.Text = "CommandFlags:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(92, 178);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 21);
+            this.label10.TabIndex = 74;
+            this.label10.Text = "使用数据库:";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDatabase.Location = new System.Drawing.Point(193, 174);
+            this.txtDatabase.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(457, 29);
+            this.txtDatabase.TabIndex = 73;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.label11.Location = new System.Drawing.Point(108, 213);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(78, 21);
+            this.label11.TabIndex = 76;
+            this.label11.Text = "推送模式:";
+            // 
+            // cmbMessageMode
+            // 
+            this.cmbMessageMode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbMessageMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMessageMode.FormattingEnabled = true;
+            this.cmbMessageMode.Location = new System.Drawing.Point(193, 209);
+            this.cmbMessageMode.Name = "cmbMessageMode";
+            this.cmbMessageMode.Size = new System.Drawing.Size(457, 29);
+            this.cmbMessageMode.TabIndex = 75;
             // 
             // cmbCommandFlags
             // 
@@ -182,33 +235,43 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbCommandFlags.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbCommandFlags.FormattingEnabled = true;
-            this.cmbCommandFlags.Location = new System.Drawing.Point(194, 208);
+            this.cmbCommandFlags.Location = new System.Drawing.Point(194, 279);
             this.cmbCommandFlags.Name = "cmbCommandFlags";
             this.cmbCommandFlags.Size = new System.Drawing.Size(457, 29);
             this.cmbCommandFlags.TabIndex = 71;
             // 
-            // label10
+            // label12
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(48, 177);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(138, 21);
-            this.label10.TabIndex = 74;
-            this.label10.Text = "使用数据库(0-15):";
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(66, 248);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(120, 21);
+            this.label12.TabIndex = 78;
+            this.label12.Text = "轮询间隔(毫秒):";
             // 
-            // txtDatabase
+            // nupMessageMode
             // 
-            this.txtDatabase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.nupMessageMode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtDatabase.Location = new System.Drawing.Point(193, 173);
-            this.txtDatabase.Maximum = new decimal(new int[] {
-            15,
+            this.nupMessageMode.Location = new System.Drawing.Point(193, 244);
+            this.nupMessageMode.Maximum = new decimal(new int[] {
+            2147483647,
             0,
             0,
             0});
-            this.txtDatabase.Name = "txtDatabase";
-            this.txtDatabase.Size = new System.Drawing.Size(457, 29);
-            this.txtDatabase.TabIndex = 73;
+            this.nupMessageMode.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nupMessageMode.Name = "nupMessageMode";
+            this.nupMessageMode.Size = new System.Drawing.Size(457, 29);
+            this.nupMessageMode.TabIndex = 77;
+            this.nupMessageMode.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
             // 
             // UIRedisMQ
             // 
@@ -219,6 +282,7 @@
             this.gpSetting.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.txtExpir)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtDatabase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupMessageMode)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -236,8 +300,12 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.CheckBox chkUseQueue;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.ComboBox cmbCommandFlags;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.NumericUpDown txtDatabase;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox cmbMessageMode;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.NumericUpDown nupMessageMode;
+        private System.Windows.Forms.ComboBox cmbCommandFlags;
     }
 }
