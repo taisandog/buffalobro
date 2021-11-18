3a82388e4095267d87b63c64ccb7054dd2ef2ccb
taisandog@gmail.com <taisandog@gmail.com>
2021-11-18 14:48:19 +0800

    提交带阻塞线程的其他工具源码

---
+++ SourceCode/其他工具源码/TreadPoolManager/BlockThread.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Buffalo.Kernel.TreadPoolManager
+{
+
+
+    /// <summary>
+    /// 带阻塞的线程
+    /// </summary>
+    public class BlockThread : IDisposable
+    {
+        /// <summary>
+        /// 无参函数
+        /// </summary>
+        private ThreadStart _runMethod;
+        /// <summary>
+        /// 带参函数
+        /// </summary>
+        private ParameterizedThreadStart _runParamMethod;
+        private BlockThread( ThreadStart method, ParameterizedThreadStart paramMethod) 
+        {
+            _thd = new Thread(new ParameterizedThreadStart(RunMethod));
+            _runMethod = method;
+            _runParamMethod = paramMethod;
+        }
+        
+
+        /// <summary>
+        /// 创建线程信息
+        /// </summary>
+        /// <param name="thd"></param>
+        /// <returns></returns>
+        public static BlockThread Create(ThreadStart method) 
+        {
+            return new BlockThread(method,null);
+        }
+        /// <summary>
+        /// 创建线程信息
+        /// </summary>
+        /// <param name="thd"></param>
+        /// <returns></returns>
+        public static BlockThread Create(ParameterizedThreadStart method)
+        {
+            return new BlockThread( null, method);
+        }
+        /// <summary>
+        /// 执行函数
+        /// </summary>
+        /// <param name="args"></param>
+        private static void RunMethod(object args)
+        {
+            BlockThread info = args as BlockThread;
+            if (info == null)
+            {
+                return;
+            }
+            try
+            {
+                if (info._runMethod != null)
+                {
+                    info._runMethod();
+                }
+                else if (info._runParamMethod!=null)
+                {
+                    info._runParamMethod(info._args);
+                }
+            }
+            finally
+            {
+                info._isRunning = false;
+                info.UnLock();
+            }
+        }
+
+        /// <summary>
+        /// 开启线程
+        /// </summary>
+        public void StartThread(object args=null) 
+        {
+            _args = args;
+            _isRunning = true;
+            _autoHandle = new AutoResetEvent(true);
+            _autoHandle.Reset();
+            
+            _thd.Start(this);
+        }
+        /// <summary>
+        /// 关闭线程
+        /// </summary>
+        public void StopThread() 
+        {
+            _isRunning = false;
+            bool isStop = false;
+            if (_autoHandle != null) 
+            {
+                isStop = _autoHandle.WaitOne(10000);
+            }
+            _autoHandle = null;
+            if (!isStop && _thd != null && _thd.IsAlive) 
+            {
+                try
+                {
+                    _thd.Abort();
+                }
+                catch { }
+            }
+            _thd = null;
+        }
+        /// <summary>
+        /// 告诉线程要关闭
+        /// </summary>
+        public void SendThread()
+        {
+            _isRunning = false;
+            
+        }
+        /// <summary>
+        /// 通知线程已经执行完
+        /// </summary>
+        public void UnLock() 
+        {
+            if (_autoHandle != null)
+            {
+                _autoHandle.Set();
+            }
+        }
+        private object _args;
+
+        private bool _isRunning;
+
+        private Thread _thd;
+        /// <summary>
+        /// 阻塞
+        /// </summary>
+        private AutoResetEvent _autoHandle = null;
+        /// <summary>
+        /// 当前线程
+        /// </summary>
+        public Thread CurrentThread
+        {
+            get
+            {
+                return _thd;
+            }
+
+           
+        }
+
+        /// <summary>
+        /// 传入参数
+        /// </summary>
+        public object Args
+        {
+            get
+            {
+                return _args;
+            }
+
+            
+        }
+
+        /// <summary>
+        /// 本线程是否需要运行
+        /// </summary>
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+
+            
+        }
+
+        public void Dispose()
+        {
+            StopThread();
+        }
+    }
+}
---
+++ SourceCode/其他工具源码/TreadPoolManager/BlockThreadPool.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Buffalo.Kernel.TreadPoolManager
+{
+    /// <summary>
+    ///  用户线程池
+    /// </summary>
+    public class BlockThreadPool
+    {
+        /// <summary>
+        /// 线程信息
+        /// </summary>
+        private ConcurrentQueue<BlockThread> _que = new ConcurrentQueue<BlockThread>();
+
+        /// <summary>
+        /// 最后清理日期
+        /// </summary>
+        private DateTime _lastClean = DateTime.MinValue;
+        private int _checkmilliseconds = 0;
+        /// <summary>
+        /// 检查间隔
+        /// </summary>
+        public int Checkmilliseconds 
+        {
+            get 
+            {
+                return _checkmilliseconds;
+            }
+        }
+        /// <summary>
+        /// 线程过时
+        /// </summary>
+        private int _threadTimeOut = 0;
+        /// <summary>
+        /// 线程过时
+        /// </summary>
+        public int ThreadTimeOut
+        {
+            get
+            {
+                return _threadTimeOut;
+            }
+        }
+        
+       
+
+        /// <summary>
+        /// 用户线程池
+        /// </summary>
+        /// <param name="checkmilliseconds">检查间隔</param>
+        /// <param name="threadTimeOut">线程过时</param>
+        public BlockThreadPool(int checkmilliseconds=1000,int threadTimeOut=2000) 
+        {
+            _checkmilliseconds = checkmilliseconds;
+            _threadTimeOut = threadTimeOut;
+            
+        }
+
+
+        /// <summary>
+        /// 清除过时线程
+        /// </summary>
+        public void CleanTimeout()
+        {
+            DateTime now = DateTime.Now;
+            if (now.Subtract(_lastClean).TotalMilliseconds < _checkmilliseconds)
+            {
+                return;
+            }
+            BlockThread einfo = null;
+            int num = 0;
+            ConcurrentQueue<BlockThread> que = _que;
+            while (que != null && que.Count > 0)
+            {
+                if (!que.TryPeek(out einfo))
+                {
+                    break;
+                }
+                if ( einfo.IsRunning)
+                {
+                    break;
+                }
+                if (!que.TryDequeue(out einfo))
+                {
+                    break;
+                }
+                num++;
+                einfo.StopThread();
+                einfo = null;
+            }
+            _lastClean = DateTime.Now;
+        }
+
+        /// <summary>
+        /// 停止所有线程
+        /// </summary>
+        public void StopAll()
+        {
+            try
+            {
+                ConcurrentQueue<BlockThread> que = _que;
+                Queue<BlockThread> beStop = new Queue<BlockThread>();
+                if (que != null)
+                {
+                    BlockThread einfo = null;
+                    while (que.Count > 0)
+                    {
+                        if (!_que.TryDequeue(out einfo))
+                        {
+                            break;
+                        }
+                        einfo.SendThread();
+                        beStop.Enqueue(einfo);
+                    }
+                    while (beStop.Count > 0)
+                    {
+                        einfo = beStop.Dequeue();
+                        if (einfo==null)
+                        {
+                            break;
+                        }
+                        einfo.SendThread();
+                        beStop.Enqueue(einfo);
+                    }
+                }
+            }
+            catch { }
+        }
+        /// <summary>
+        /// 增加线程信息
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public BlockThread AppendThreadInfo(BlockThread info) 
+        {
+            CleanTimeout();
+            _que.Enqueue(info);
+            return info;
+        }
+        /// <summary>
+        /// 启动无参数线程
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public BlockThread RunThread(ThreadStart method)
+        {
+
+            BlockThread info = BlockThread.Create(method);
+            info.StartThread();
+            return AppendThreadInfo(info);
+        }
+        /// <summary>
+        /// 启动有参数线程
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public BlockThread RunParamThread(ParameterizedThreadStart method,object args)
+        {
+
+            BlockThread info = BlockThread.Create(method);
+            info.StartThread(args);
+            return AppendThreadInfo(info);
+        }
+
+    }
+}
