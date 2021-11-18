601c58a85fda92d225abf975f79772d3aeb1dc78
taisandog@gmail.com <taisandog@gmail.com>
2021-11-18 14:48:19 +0800

    提交带阻塞线程的其他工具源码

---
+++ SourceCode/其他工具源码/TreadPoolManager/UserThreadInfo.cs
@@ -0,0 +1,214 @@
+﻿using Buffalo.ArgCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Buffalo.Kernel.TreadPoolManager
+{
+    /// <summary>
+    /// 线程信息
+    /// </summary>
+    public class UserThreadInfo
+    {
+        private Thread _thd;
+        /// <summary>
+        /// 线程
+        /// </summary>
+        public Thread CurrentThread
+        {
+            get
+            {
+                return _thd;
+            }
+        }
+        private AutoResetEvent _handle;
+        /// <summary>
+        /// 锁的阻塞
+        /// </summary>
+        public AutoResetEvent Handle
+        {
+            get
+            {
+                return _handle;
+            }
+
+        }
+        
+        private DateTime _createDate;
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateDate
+        {
+            get
+            {
+                return _createDate;
+            }
+        }
+        /// <summary>
+        /// 无参数函数
+        /// </summary>
+        private ThreadStart _method;
+        /// <summary>
+        /// 无参数函数
+        /// </summary>
+        public ThreadStart Method 
+        {
+            get 
+            {
+                return _method;
+            }
+        }
+        /// <summary>
+        /// 带参数函数
+        /// </summary>
+        private ParameterizedThreadStart _paramMethod;
+        /// <summary>
+        /// 带参数函数
+        /// </summary>
+        public ParameterizedThreadStart ParamMethod
+        {
+            get
+            {
+                return _paramMethod;
+            }
+        }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        private object _param;
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public object Param
+        {
+            get
+            {
+                return _param;
+            }
+        }
+        /// <summary>
+        /// 押注线程信息
+        /// </summary>
+        /// <param name="thd">线程</param>
+        /// <param name="args">参数</param>
+        public UserThreadInfo()
+        {
+            _handle = new AutoResetEvent(true);
+            _handle.Reset();
+            _createDate = DateTime.Now;
+        }
+        /// <summary>
+        /// 执行无参数线程
+        /// </summary>
+        /// <param name="thd"></param>
+        public void RunThread(ThreadStart method)
+        {
+            _method = method;
+            _thd = new Thread(new ThreadStart(DoThread));
+            _thd.Start();
+        }
+        /// <summary>
+        /// 执行参数线程
+        /// </summary>
+        /// <param name="thd"></param>
+        public void RunParamThread(ParameterizedThreadStart paramMethod,object args)
+        {
+            _paramMethod = paramMethod;
+            _thd = new Thread(new ParameterizedThreadStart(DoParamThread));
+            _thd.Start(args);
+        }
+        /// <summary>
+        /// 执行无参数线程
+        /// </summary>
+        public void DoThread() 
+        {
+            try
+            {
+                _method();
+            }
+            finally 
+            {
+                UnLock();
+            }
+        }
+        /// <summary>
+        /// 执行有参数线程
+        /// </summary>
+        public void DoParamThread(object args)
+        {
+            try
+            {
+                _paramMethod(args);
+            }
+            finally
+            {
+                UnLock();
+            }
+        }
+        /// <summary>
+        /// 清空信息
+        /// </summary>
+        public void Clear()
+        {
+            _thd = null;
+            
+            if (_handle != null)
+            {
+                _handle.Close();
+                _handle.Dispose();
+            }
+            _handle = null;
+        }
+        public void UnLock()
+        {
+            if (_handle != null)
+            {
+                _handle.Set();
+            }
+        }
+        /// <summary>
+        /// 是否已经结束
+        /// </summary>
+        public bool IsRunning
+        {
+            get 
+            {
+                return _thd != null && _thd.IsAlive;
+            }
+        }
+
+        /// <summary>
+        /// 关闭线程
+        /// </summary>
+        /// <returns></returns>
+        public APIResault CloseThread()
+        {
+            bool hasClose = false;
+            if (_handle == null || _thd == null)
+            {
+                return MU.GetSuccessValue(null, hasClose);
+            }
+            
+            if (!_handle.WaitOne(100))
+            {
+                try
+                {
+                    _thd.Abort();
+                    Thread.Sleep(10);
+                    hasClose = true;
+                }
+                catch (Exception ex)
+                {
+                    return MU.GetFault(ex.ToString());
+                }
+            }
+            Clear();
+            return MU.GetSuccessValue(null, hasClose);
+        }
+    }
+}
---
+++ SourceCode/其他工具源码/TreadPoolManager/UserThreadPool.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Buffalo.Kernel.TreadPoolManager
+{
+    /// <summary>
+    ///  用户线程池
+    /// </summary>
+    public class UserThreadPool
+    {
+        /// <summary>
+        /// 线程信息
+        /// </summary>
+        private ConcurrentQueue<UserThreadInfo> _que = new ConcurrentQueue<UserThreadInfo>();
+
+        /// <summary>
+        /// 最后清理日期
+        /// </summary>
+        private DateTime _lastClean = DateTime.MinValue;
+        private int _checkmilliseconds = 0;
+        /// <summary>
+        /// 检查间隔
+        /// </summary>
+        public int Checkmilliseconds 
+        {
+            get 
+            {
+                return _checkmilliseconds;
+            }
+        }
+        /// <summary>
+        /// 线程过时
+        /// </summary>
+        private int _threadTimeOut = 0;
+        /// <summary>
+        /// 线程过时
+        /// </summary>
+        public int ThreadTimeOut
+        {
+            get
+            {
+                return _threadTimeOut;
+            }
+        }
+        
+       
+
+        /// <summary>
+        /// 用户线程池
+        /// </summary>
+        /// <param name="checkmilliseconds">检查间隔</param>
+        /// <param name="threadTimeOut">线程过时</param>
+        public UserThreadPool(int checkmilliseconds=1000,int threadTimeOut=2000) 
+        {
+            _checkmilliseconds = checkmilliseconds;
+            _threadTimeOut = threadTimeOut;
+            
+        }
+
+
+        /// <summary>
+        /// 清除过时线程
+        /// </summary>
+        public void CleanTimeout()
+        {
+            DateTime now = DateTime.Now;
+            if (now.Subtract(_lastClean).TotalMilliseconds < _checkmilliseconds)
+            {
+                return;
+            }
+            UserThreadInfo einfo = null;
+            int num = 0;
+            ConcurrentQueue<UserThreadInfo> que = _que;
+            while (que != null && que.Count > 0)
+            {
+                if (!que.TryPeek(out einfo))
+                {
+                    break;
+                }
+                if (now.Subtract(einfo.CreateDate).TotalMilliseconds < ThreadTimeOut && einfo.IsRunning)
+                {
+                    break;
+                }
+                if (!que.TryDequeue(out einfo))
+                {
+                    break;
+                }
+                num++;
+                einfo.CloseThread();
+                einfo = null;
+            }
+            _lastClean = DateTime.Now;
+        }
+
+        /// <summary>
+        /// 清空所有线程
+        /// </summary>
+        public void CleanAll()
+        {
+            try
+            {
+                ConcurrentQueue<UserThreadInfo> que = _que;
+                if (que != null)
+                {
+                    UserThreadInfo einfo = null;
+                    while (que.Count > 0)
+                    {
+                        if (!_que.TryDequeue(out einfo))
+                        {
+                            break;
+                        }
+                        einfo.CloseThread();
+                    }
+                    
+                }
+            }
+            catch { }
+        }
+        /// <summary>
+        /// 增加线程信息
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public UserThreadInfo AppendThreadInfo(UserThreadInfo info) 
+        {
+            CleanTimeout();
+            _que.Enqueue(info);
+            return info;
+        }
+        /// <summary>
+        /// 启动无参数线程
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public UserThreadInfo RunThread(ThreadStart method)
+        {
+            
+            UserThreadInfo info = new UserThreadInfo();
+            info.RunThread(method);
+            return AppendThreadInfo(info);
+        }
+        /// <summary>
+        /// 启动有参数线程
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public UserThreadInfo RunParamThread(ParameterizedThreadStart method,object args)
+        {
+
+            UserThreadInfo info = new UserThreadInfo();
+            info.RunParamThread(method, args);
+            return AppendThreadInfo(info);
+        }
+
+    }
+}
